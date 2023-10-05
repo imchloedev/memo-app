e905a874a6d51aca6a00c6d6ae0b1ee0be961e07
Eunji Kang <etothej@naver.com>
2023-10-05 12:59:26 +0900

    style: search input icon 추가

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -6,6 +6,7 @@ import { styled } from "styled-components/native";
 import NoteItem from "~/components/notes/NoteItem";
 import Layout from "~/components/common/Layout";
 import ScreenTitle from "~/components/common/ScreenTitle";
+import Icon from "react-native-vector-icons/AntDesign";
 import { useSearchNotesQuery } from "hooks/notes";
 import { MainStackParamList } from "../@types";
 
@@ -27,6 +28,7 @@ const Search = ({ navigation }: SearchScreenProps) => {
           <ScreenTitle title="Search" />
         </TitleWrapper>
         <InputWrapper>
+          <Icon name="search1" color="#777" size={16} />
           <Input
             value={text}
             onChangeText={setText}
@@ -74,11 +76,16 @@ const InputWrapper = styled.View`
   background-color: ${({ theme }) => theme.color.container};
   border-radius: 20px;
   margin-bottom: 10px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
+  padding: 18px 20px;
 `;
 
 const Input = styled.TextInput`
   color: ${({ theme }) => theme.color.textColor};
-  padding: 18px 20px;
+  flex-shrink: 1;
 `;
 
 const ResultWrapper = styled.View`
